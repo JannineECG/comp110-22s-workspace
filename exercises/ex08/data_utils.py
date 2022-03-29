55eaf8a52c196d91400240a5200b72f292ddb04c
Jannine Castro <jannine@ad.unc.edu>
2022-03-29 16:41:44 -0400

    ex08

---
+++ exercises/ex08/data_utils.py
@@ -0,0 +1,67 @@
+"""This course is dependant on TA's  therefore students are struggling to understand the concepts."""
+
+__author__ = "730470181"
+
+from csv import DictReader
+
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read the rows of a csv into a 'table'."""
+    result: list[dict[str, str]] = []
+    file_handle = open(filename, "r", encoding="utf8")
+    csv_reader = DictReader(file_handle)
+    for row in csv_reader:
+        result.append(row)
+    file_handle.close()
+    return result
+
+
+def column_values(table: list[dict[str, str]], column: str) -> list[str]:
+    """Produce a list[str] of all values in a single column whose name is the second parameter."""
+    end: list[str] = []
+    for row in table: 
+        item: str = row[column]
+        end.append(item)
+    return end
+
+
+def columnar(row_table: list[dict[str, str]]) -> dict[str, list[str]]:
+    """Transform a row-oriented table to a column-oriented table."""
+    result: dict[str, list[str]] = {}
+    first_row: dict[str, str] = row_table[0]
+    for column in first_row:
+        result[column] = column_values(row_table, column)
+    return result
+
+
+def head(cols: dict[str, list[str]], N: int) -> dict[str, list[str]]:
+    """Create a column-based table with the fist N row of data for each column."""
+    end: dict[str, list[str]] = {}
+    for key in cols:
+        xs: list[str] = []
+        i: int = 0
+        while i < N:
+            if len(cols[key]) > i:
+                xs.append(cols[key][i])
+            i += 1
+        end[key] = xs
+    return end
+
+
+def select(cols: dict[str, list[str]], name: list[str]) -> dict[str, list[str]]:
+    """Selecting columns I only want."""
+    end: dict[str, list[str]] = {}
+    for key in name:
+        end[key] = cols[key]
+    return end
+
+
+def count(fre: list[str]) -> dict[str, int]:
+    """The data of difficulty of the course, how well students are undertsanding the course, the pace, office hours visit, and effectiveness."""
+    end: dict[str, int] = {}
+    for key in fre:
+        if key in end:
+            end[key] += 1
+        else:
+            end[key] = 1
+    return end
